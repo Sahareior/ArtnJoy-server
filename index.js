de9dcfbd339d4583e7db555b1362b12f310cea4b
mdAshaduzzaman1 <ashaduzzamannoor19@gmail.com>
2023-06-15 22:29:52 +0600

    fixed some bugs

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const stripe = require('stripe')('sk_test_51NEo1rESJveeRr2D7C6p7latFyClahoQ1dj9pw2qLzm7D088AbwYkUkY4l3LIT9NPStLZseQMGOlUCqWxXJvRGwf00BQatOtqb');
 const cors = require('cors')
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const app = express()
 app.use(cors())
 app.use(express.json());
-const secret = '61bcd85b905e1553f039bfb036f5a4793bcab874f1752b481bdb9c9f52f134f8'
+// const secret = '61bcd85b905e1553f039bfb036f5a4793bcab874f1752b481bdb9c9f52f134f8'
 
 const port = process.env.PORT|| 5000
 
@@ -19,7 +20,7 @@ const verifyJWT = (req, res, next) => {
   const token = authorization.split(' ')[1];
   // console.log(token)
 
-  jwt.verify(token, secret, (err, decoded) => {
+  jwt.verify(token, process.env.SECRET_CODE, (err, decoded) => {
     if (err) {
       return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
@@ -30,7 +31,7 @@ const verifyJWT = (req, res, next) => {
 // art&craft KrLszqKj2aBRnNeL
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://artANDcraft:KrLszqKj2aBRnNeL@cluster0.j0rll9s.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.j0rll9s.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -44,23 +45,38 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     client.connect();
     const userCollections = client.db('carftANDartDB').collection('users')
     const instructorCollections = client.db('carftANDartDB').collection('instructor')
     const cartCollections = client.db('carftANDartDB').collection('carts')
     const classCollections = client.db('carftANDartDB').collection('AddedClasses')
     const paymentCollections = client.db('carftANDartDB').collection('payments')
     const studentCollections = client.db ('carftANDartDB').collection('students')
+    const instructorList = client.db('carftANDartDB').collection('insList')
+    const classesList =  client.db('carftANDartDB').collection('classes')
 
 
 
     
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, secret, { expiresIn: '1h' })
+      const token = jwt.sign(user, process.env.SECRET_CODE, { expiresIn: '1h' })
 
       res.send({ token })
     })
+
+
+    app.get('/classes',async (req,res)=>{
+      const cursor = classesList.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+    app.get('/instructor',async (req,res)=>{
+      const cursor = instructorList.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
 
     app.post('/users',  async (req, res) => {
         const newUser = req.body;
